fbcfd7c3778e5b43abfb9eee1d87325e88901741
Daddyjohn63 <john@webandprosper.co.uk>
2024-02-06 16:45:18 +0000

    clerk auth created

--- app/(auth)/(routes)/sign-in/page.jsx
+++
@@ -1,9 +0,0 @@
-const SignInPage = () => {
-  return (
-    <div>
-      <h1>This is an unprotected page</h1>
-    </div>
-  );
-};
-
-export default SignInPage;
--- app/(dashboard)/(routes)/page.js
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button';
-
-export default function Home() {
-  return (
-    <div>
-      <p>this is a protected page</p>
-    </div>
-  );
-}
