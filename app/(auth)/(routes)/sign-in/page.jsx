a3819af070ccae8d9f32919fb7c133b7e0927205
Daddyjohn63 <john@webandprosper.co.uk>
2024-02-06 15:59:57 +0000

    first commt

---
+++ app/(auth)/(routes)/sign-in/page.jsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return (
+    <div>
+      <h1>This is an unprotected page</h1>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ app/(dashboard)/(routes)/page.js
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <p>this is a protected page</p>
+    </div>
+  );
+}
