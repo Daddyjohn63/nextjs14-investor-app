7464bc6cc59a9fdea3faae9c9d7b0d71586315df
Daddyjohn63 <john@webandprosper.co.uk>
2024-02-08 17:05:50 +0000

    course creation UI

--- app/(dashboard)/(routes)/edit/create-company/page.jsx
+++ app/(dashboard)/(routes)/edit/create-company/page.jsx
@@ -1,4 +1,88 @@
+// import { Button } from '@/components/ui/button';
+// import Link from 'next/link';
+
+'use client';
+
+import * as Z from 'zod';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
+
+const formSchema = Z.object({
+  title: Z.string().min(1, {
+    message: 'Title is Required'
+  })
+});
+
 const createCompanyPage = () => {
-  return <div> Create a company page</div>;
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: ''
+    }
+  });
+  //extract the state from the form
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async values => {
+    try {
+      const response = await axios.post('/api/company', values);
+      router.push(`/edit/companies/${response.data.id}`);
+    } catch {
+      toast.error('Sorry, but something went wrong');
+    }
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">Name your course</h1>
+        <p className="text-sm text-slate-600">Name of company? Don&apos;t worry, you can change this later.</p>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-8">
+            <FormField
+              control={form.control}
+              name="companyname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company name</FormLabel>
+                  <FormControl>
+                    <Input disabled={isSubmitting} placeholder="Company name" {...field} />
+                  </FormControl>
+                  <FormDescription>Please add the companies name.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+
+    // <div>
+    //   <Link href="/edit/create-company">
+    //     <Button className="p-6">New Company</Button>
+    //   </Link>
+    // </div>
+  );
 };
 export default createCompanyPage;
